92100655a9081fb326e17bc2e92f211f4bc03e6e
Murali Thangamuthu <murali.tmuthu@gmail.com>
2025-07-07 22:20:52 +1000

    Refactor code structure for improved readability and maintainability

--- apps/docs/app/_meta.js
+++
@@ -1,9 +0,0 @@
-export default {
-  index: 'Introduction',
-  'getting-started': 'Getting Started',
-  features: 'Features',
-  'user-guide': 'User Guide',
-  api: 'API Reference',
-  troubleshooting: 'Troubleshooting',
-  faq: 'FAQ'
-};
